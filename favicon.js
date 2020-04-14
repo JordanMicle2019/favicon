892494d4a163e75b97ba87dea9a5b505187d0d57
JordanMicle <JordanMicle2019@gmail.com>
2020-04-15 01:53:56 +0800

    test

--- favicon.js
+++ favicon.js
@@ -1,1 +1,4 @@
-document.querySelector("link[rel*='icon']").href = "data:,";
+var link = document.createElement("link");
+link.rel = "icon";
+link.href = "data:,";
+document.getElementsByTagName("head")[0].appendChild(link);
